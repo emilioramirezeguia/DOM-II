f70e6b41d1c6c27d136c8e6746e2f722216c4115
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-06-02 12:54:20 -0700

    Create 'keydown' event listener

--- js/index.js
+++ js/index.js
@@ -7,3 +7,17 @@ logo.addEventListener("mouseover", function (event) {
   logo.style.backgroundColor = "black";
   logo.style.color = "white";
 });
+
+// When hitting enter, momentarily clear the body, paint it dark blue, and add an h1 that says "where did everything go"
+document.body.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    document.body.innerHTML = "";
+    document.body.style.backgroundColor = "blue";
+    const h1 = document.createElement("h1");
+    h1.innerHTML = "Where did everybody go? 😔";
+    h1.style.color = "white";
+    h1.style.fontSize = "6rem";
+    h1.style.textAlign = "center";
+    document.body.appendChild(h1);
+  }
+});
