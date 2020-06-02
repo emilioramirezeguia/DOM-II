8fc63ea9f0dbfc22e646afb7a5e38513ef95f22a
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-06-02 12:25:24 -0700

    Create 'mouseover' event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+// Grab the h1 element with the logo name
+const logo = document.querySelector("h1");
+// When the cursor hovers over that element, it's background color will turn black and the text color white
+logo.addEventListener("mouseover", function (event) {
+  logo.style.backgroundColor = "black";
+  logo.style.color = "white";
+});
